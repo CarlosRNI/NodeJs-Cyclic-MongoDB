fd5a0e2edce056a9720fe2bdc2aec9efb28782b4
CarlosRNI <carlos@rnicontabilidade.com.br>
2024-02-05 16:13:02 -0300

    update

--- server.js
+++ server.js
@@ -90,6 +90,26 @@ app.post('/adicionar-cadastro', async (req, res) => {
       }
 });
 
+// Update data in the Cadastros collection based on the received data
+app.post('/update-cadastros', async (req, res) => {
+      try {
+          const { updatedData } = req.body;
+  
+          // Iterate through the updated data and update the corresponding records in the database
+          for (const updatedItem of updatedData) {
+              const { matricula, nome } = updatedItem;
+  
+              // Find the document by matricula and update the nome field
+              await Cadastros.findOneAndUpdate({ matricula }, { nome });
+          }
+  
+          res.send("Data updated successfully");
+      } catch (error) {
+          console.error(error);
+          res.status(500).send("Internal Server Error");
+      }
+  });
+
 connectDB().then(() => {
       app.listen(PORT, () => {
             console.log('Listening on port: ', PORT);
